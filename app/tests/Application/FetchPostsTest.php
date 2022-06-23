bc0a912f735cc3b425830bff22f1e6bee8f1b5f3
jamarboledas <jamarboledas@gmail.com>
2022-06-23 07:51:33 +0200

    tests have been improved

--- app/tests/Application/FetchPostsTest.php
+++ app/tests/Application/FetchPostsTest.php
@@ -7,7 +7,10 @@ use PHPUnit\Framework\TestCase as KernelTestCase;
 
 final class FetchPostsTest extends KernelTestCase
 {
-    public function testFetchTenPostsReturnAnArrayOfMaximunTenPosts(): void
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testFetchTenPostsReturnAnArrayOfMaximunTenPosts($filter, $maxElements): void
     {
         $twitterOauth = new TwitterOAuth(
             getenv('CONSUMER_KEY'), 
@@ -18,7 +21,61 @@ final class FetchPostsTest extends KernelTestCase
 
         $fetchInterface = new TwitterClient($twitterOauth);
         $fetchPost = new FetchPosts($fetchInterface);
-        $tweets = $fetchPost->fetch('hello', 10);
-        $this->assertLessThanOrEqual(10, count($tweets));
+        $tweets = $fetchPost->fetch($filter, $maxElements);
+        $this->assertLessThanOrEqual($maxElements, count($tweets));
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testFetchPostsWithZeroAsMaxElementsReturnsFifteenElementsOrLess($filter): void
+    {
+        $twitterOauth = new TwitterOAuth(
+            getenv('CONSUMER_KEY'), 
+            getenv('CONSUMER_SECRET'),
+            getenv('ACCESS_TOKEN'),
+            getenv('ACCESS_TOKEN_SECRET')
+        );
+
+        $fetchInterface = new TwitterClient($twitterOauth);
+        $fetchPost = new FetchPosts($fetchInterface);
+        $tweets = $fetchPost->fetch($filter, 0);
+        $this->assertLessThanOrEqual(15, count($tweets));
+    }
+
+    /**
+     * @dataProvider invalidDataProvider
+     */
+    public function testFetchPostsWithInvalidArgumentAsMaxElementsReturnsFalse($filter, $maxElements): void
+    {
+        $twitterOauth = new TwitterOAuth(
+            getenv('CONSUMER_KEY'), 
+            getenv('CONSUMER_SECRET'),
+            getenv('ACCESS_TOKEN'),
+            getenv('ACCESS_TOKEN_SECRET')
+        );
+
+        $fetchInterface = new TwitterClient($twitterOauth);
+        $fetchPost = new FetchPosts($fetchInterface);
+        $tweets = $fetchPost->fetch($filter, $maxElements);
+        $this->assertFalse($tweets);
+    }
+
+    public function dataProvider()
+    {
+        return array(
+          array('hello', 5),
+          array('this', 10),
+          array('is', 20),
+          array('a', 30),
+          array('test', 40)
+        );
+    }
+
+    public function invalidDataProvider()
+    {
+        return array(
+          array('less than zero', -5)
+        );
     }
 }
